554d9e0a1eefa53998e34ab757550b73bca62b24
razileon <65210958+razileon@users.noreply.github.com>
2023-11-11 03:08:26 +0800

    adding files

---
+++ assets/Scene13-a7166924.js
@@ -0,0 +1,1 @@
+import{o as e,c as s,a,w as t,Q as c,d as o}from"./index-953b5b8a.js";const n={class:"d-flex flex-column h-100"},l=o("h1",{class:"row align-self-center"},"The end.",-1),h={__name:"Scene13",setup(r){return(_,i)=>(e(),s("div",n,[a(c,null,{image:t(()=>[l]),_:1})]))}};export{h as default};
